d50c0bdc193ab6584a4ba490d94aeb0bf8c2cc1b
Jon Pugh <jon@thinkdrop.net>
2015-12-23 14:27:07 -0500

    only one v is needed

--- apt-upgrade.sh
+++ apt-upgrade.sh
@@ -1,2 +1,2 @@
 export ANSIBLE_FORCE_COLOR=true
-ansible-playbook playbooks/apt-upgrade.yml -i devshop-inventory.php --extra-vars "devshop_version=$DEVSHOP_VERSION" -vv
+ansible-playbook playbooks/apt-upgrade.yml -i devshop-inventory.php --extra-vars "devshop_version=$DEVSHOP_VERSION" -v
