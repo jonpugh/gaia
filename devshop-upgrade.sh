b1ccba29a857c85dab5ffe47a540223f7ea80293
Jon Pugh <jon@thinkdrop.net>
2015-12-23 22:45:29 -0500

    getting rid of crazy unnecessary version parameters

--- devshop-upgrade.sh
+++ devshop-upgrade.sh
@@ -1,11 +1,2 @@
-if [ $1 ]; then
-    DEVSHOP_VERSION=$1
-else
-    DEVSHOP_VERSION='0.x'
-fi
-
 export ANSIBLE_FORCE_COLOR=true
-
-echo "INSTALLING VERSION: $DEVSHOP_VERSION"
-ansible-playbook playbooks/devshop-upgrade.yml -i devshop-inventory.php --extra-vars "devshop_version=$DEVSHOP_VERSION" -v
-
+ansible-playbook playbooks/devshop-upgrade.yml -i devshop-inventory.php
